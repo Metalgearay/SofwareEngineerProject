9d81eb48e0bdc9272837c24603301b0972375071
Soul Raj Joshi <souljoshi@dyn-160-39-133-230.dyn.columbia.edu>
2016-10-04 15:47:31 -0400

    added more jasmine tests

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -1,16 +1,33 @@
 var request = require("request");
-var ok = require("../app.js");
+var app = require("../app.js");
 var base_url = "http://localhost:8080/";
 
 describe("200 OK", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-	ok.closeServer();
-        done();
-      });
+        request.get(base_url, function(error, response, body) {
+            expect(response.statusCode).toBe(200);
+            //app.closeServer();
+            done();
+        });
     });
+
+    it("contains the the string Game", function(done) {
+        request.get(base_url, function(error, response, body) {
+            expect(body).toContain('Game');
+            //app.closeServer();
+            done();
+        });
+    });
+
+    it("has title tag", function(done) {
+        request.get(base_url, function(error, response, body) {
+            expect(body).toContain('<title>');
+            app.closeServer();
+            done();
+        });
+    });
+
 
   });
 });
